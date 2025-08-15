bc3fb1604c736f5aaa1e775ce89f2bd3cc7e2588
yqq-a <qiuchanye7@gmail.com>
2025-08-15 15:57:45 +0800

    Add ESLint configuration with React and TypeScript rules

--- eslint.config.js
+++ eslint.config.js
@@ -23,8 +23,9 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
     },
   },
 )
