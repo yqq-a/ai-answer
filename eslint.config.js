6588e61ec4f2a3af0f1d5a3e46ebcfddc4158fd1
yqq-a <qiuchanye7@gmail.com>
2025-08-15 15:52:15 +0800

    Add ESLint configuration for AI Answer project

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
